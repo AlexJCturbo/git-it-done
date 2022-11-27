fb09b1fc3c67ae5057fb4fa5c7bbebc8177ab043
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-11-27 01:43:12 -0600

    removed some comments

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -19,18 +19,16 @@ var getFeaturedRepos = function (language) {
     }
   })
 }
-//getFeaturedRepos('javascript');
+
 
 
 var buttonClickHandler = function (event) {
-  //This function must use event delegation to handle all clicks on buttons
-  //The browser's event object will have a target property that tells us exactly which HTML element was interacted with to create the event
+  //The browser's event object will have a target property to identify which HTML element was interacted with to create the event
   var language = event.target.getAttribute('data-language');
   //console.log(language);
 
   if (language) {
     getFeaturedRepos(language);
-
     //Clear old content
     repoContainerEl.textContent = "";
   }
@@ -38,12 +36,11 @@ var buttonClickHandler = function (event) {
 
 
 var formSubmitHandler = function (event) {
-  //event.preventDefault() stops the browser from performing the default action the event wants it to do. In the case of submitting a form, it prevents the browser from sending the form's input data to a URL, as we'll handle what happens with the form input data ourselves in JavaScript.
+  //event.preventDefault() stops the browser from performing the default action the event wants it to do. In the case of submitting a form, it prevents the browser from sending the form's input data to a URL.
   event.preventDefault();
   //console.log(event);
 
   //Get value from input element
-  //Get the value from the <input> element via the nameInputEl DOM variable and store the value in its own variable called username
   var username = nameInputEl.value.trim();
 
   //check that there's a value in that username variable.
@@ -57,24 +54,15 @@ var formSubmitHandler = function (event) {
 }
 
 
-//var getUserRepos = function () {
 var getUserRepos = function (user) {
-
   //Format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos"
-
-  //HTTP request to the GitHub API using fetch()
-  //the request originated from the app, and the response came from the server.
-  //fetch() method is asynchronous
-  //This kind of asynchronous communication with a server is often referred to as AJAX, which stands for Asynchronous JavaScript and XML
-  //fetch("https://api.github.com/users/alexJCturbo/repos")
 
   //Make a request to the url
   fetch(apiUrl)
     .then(function (response) {
-      //check if it was a successful request by using the ok property. When the HTTP request status code is something in the 200s, the ok property will be true.
+      //check if it was a successful request by using the ok property
       if (response.ok) {
-        //Before we can use the data in the code, we need to format the response using the json() method to format the response as JSON. Note: sometimes a resource may return non-JSON data. In those cases a different method, like text(), would be used
         response.json()
           //The json() method returns another Promise, hence the extra then() method, whose callback function captures the actual data.
           .then(function (data) {
@@ -82,15 +70,12 @@ var getUserRepos = function (user) {
             //console.log(data)
           });
       } else {
-        //If the ok property bundled to the response is false, we know that something is wrong with the HTTP request, so we set a custom response/alert to let the user know that their search was unsuccessful.
+        //If the ok property bundled to the response is false, then something is wrong
         alert('Error: GitHub User Not Found')
       }
-      //console.log("inside", response);
     })
     .catch(function (error) {
-      //Notice this `.catch()` getting chained onto the end of the `.then()` method. This the Fetch API's way of handling network errors.
       alert("Unable to connect to GitHub");
-      //When we use fetch() to create a request, the request might go one of two ways: the request may find its destination URL and attempt to get the data in question, which would get returned into the .then() method; or if the request fails, that error will be sent to the .catch() method.
     });
 };
 
@@ -101,7 +86,7 @@ var displayRepos = function (repos, searchTerm) {
     repoContainerEl.textContent = 'No repositories found.'
     return;
   }
-  //When working with an app that displays data based on user input, we should always clear out the old content before displaying new content.
+  //Clear out the old content before displaying new content.
   repoContainerEl.textContent = '';
   repoSearchTerm.textContent = searchTerm;
 
@@ -115,16 +100,12 @@ var displayRepos = function (repos, searchTerm) {
     repoEl.classList = "list-item flex-row justify-space-between align-center";
     //Create a link for each repo
     repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
     //Create a <span> element to hold the repository name
     var titleEl = document.createElement("span");
-
     //Write the name of the repository into the <span> element
     titleEl.textContent = repoName;
-
     //Append <span> to the <div> in the container
     repoEl.append(titleEl);
-
     //Create a status element
     var statusEl = document.createElement('span');
     statusEl.classList = 'flex-row align-center';
@@ -138,19 +119,12 @@ var displayRepos = function (repos, searchTerm) {
 
     //Append issues <span> element to the container
     repoEl.appendChild(statusEl);
-
     //Append new <div> into existing  results <div id="repos-container"> to the DOM
     repoContainerEl.appendChild(repoEl);
   }
-  //In the for loop, we're taking each repository (repos[i]) and writing some of its data to the page. First we format the appearance of the name and repository name. Next we create and style a <div> element. Then we create a <span> to hold the formatted repository name. We add that to the <div> and add the entire <div> to the container we created earlier.
-
-  console.log(repos);
-  console.log(searchTerm);
+  // console.log(repos);
+  // console.log(searchTerm);
 }
 
-//We delegate the click handling of the language buttons to their parent element
 languageButtonsEl.addEventListener('click', buttonClickHandler);
-
 userFormEl.addEventListener('submit', formSubmitHandler);
-//getUserRepos('alexJCturbo');
-//console.log('outside')
--- assets/js/single.js
+++ assets/js/single.js
@@ -3,71 +3,57 @@ var limitWarningEl = document.querySelector('#limit-warning');
 var repoNameEl = document.querySelector('#repo-name');
 
 var getRepoName = function () {
-  //Assigning the query string to a variable (in this case the query string format looks like: ?repo="username"/"repo")
-  //The .location contains information of the current URL and .search returns the URL query (including the "?")
   //Grab repo name from url query string
   var queryString = document.location.search;
-  //After split we have an array with 2 elements. We retieve the second using the [1] index.
+  //Retieve the array second element using the [1] index.
   var repoName = queryString.split('=')[1];
-  //console.log(repoName);
 
   if (repoName) {
     //Display  the repo name to the header
     repoNameEl.textContent = repoName;
-
     //Passing the username and repo to the getRepoIssues() function
     getRepoIssues(repoName)
   } else {
     //If no repo was given, redirect to the homepage
     document.location.replace('./index.html');
   }
-
 };
+
 
 var displayWarning = function (repo) {
   //Add text to warning container
   limitWarningEl.textContent = 'To see more than 30 issues, visit ';
-
   //Create an <a> element to add the link to the repo issues
   var linkEl = document.createElement('a');
-
   //Append a link element with an href attribute that points to https://github.com/<repo>/issues
   linkEl.textContent = 'see more issues on GitHub.com';
   linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
   linkEl.setAttribute('target', '_blank');
-
   //Append your link to the warning container
   limitWarningEl.appendChild(linkEl);
 }
 
 
 var getRepoIssues = function (user_repo) {
-  //Option to append ?direction=asc to the end of the query URL to specify the sort order to return older issues first.
+  //Option to append ?direction=asc to the end of the query URL to return older issues first.
   var apiUrl = "https://api.github.com/repos/" + user_repo + "/issues?direction=asc";
-  fetch(apiUrl)
-    .then(function (response) {
-      //Request was successful
-      if (response.ok) {
-        response.json()
-          .then(function (data) {
-            //Pass response data to DOM function
-            displayIssues(data);
-            //console.log(data);
-
-            //Check if API has paginated issues (more than 30 issues)
-            if (response.headers.get('Link')) {
-              displayWarning(user_repo);
-              //console.log('repo has more than 30 issues')
-            }
-          })
-      } else {
-        //alert('There was a problem with your request');
-        //If not successful, redirect to homepage
-        document.location.replace('./index.html');
-      }
-    })
-  //console.log(user_repo);
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        //Pass response data to DOM function
+        displayIssues(data);
+        //Check if API has paginated issues (more than 30 issues)
+        if (response.headers.get('Link')) {
+          displayWarning(user_repo);
+        }
+      })
+    } else {
+      //If not successful, redirect to homepage
+      document.location.replace('./index.html');
+    }
+  })
 }
+
 
 var displayIssues = function (issues) {
   //Check for no issues and display a message
@@ -84,14 +70,11 @@ var displayIssues = function (issues) {
     issueEl.setAttribute('href', issues[j].html_url);
     //Added a target="_blank" attribute to each <a> element, to open the link in a new tab instead of replacing the current webpage.
     issueEl.setAttribute('target', '_blank');
-
     //Create span to hold the issue title ('title' is a key value pair coming from the GitHub API)
     var titleEl = document.createElement('span')
     titleEl.textContent = issues[j].title;
-
     //Append the <span> to the <a> element (container)
     issueEl.appendChild(titleEl)
-
     //Create a type element
     var typeEl = document.createElement('span');
 
@@ -104,15 +87,9 @@ var displayIssues = function (issues) {
 
     //Append to container <a>
     issueEl.append(typeEl);
-
     //Append to the main issues container <div id="issues-container">
     issueContainerEl.appendChild(issueEl);
   }
 }
 
 getRepoName();
-
-//getRepoIssues('facebook/react');
-//getRepoIssues('alexJCturbo/git-it-done');
-
-//facebook/react, expressjs/express, angular/angular, alexJCturbo/taskmaster, alexJCturbo/git-it-done
